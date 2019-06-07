10dbb7839e04295d3b74ed694207b91e0ffd0d27
Fredrik Eilertsen <fredrik.eilertsen@gmail.com>
2019-06-07 16:07:38 +0200

    Add working example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { CropperComponent } from 'cropit';
+
 
 @Component({
   selector: 'app-root',
@@ -8,14 +11,78 @@ import { CropperComponent } from 'cropit';
 })
 
 export class AppComponent {
-  title = 'CropIt';
 
-  @ViewChild("angularCropper", {static:true}) public angularCropper: CropperComponent;
+  form: FormGroup = this.fb.group({
+    avatar: null
+  });
 
-  mordi(event) {
+  loading: boolean = false;
 
-    var file = event["srcElement"]["files"][0];
+  imageUrl = "https://fengyuanchen.github.io/cropperjs/images/picture.jpg"
 
-    var mordi = this.angularCropper.cropper.getCanvasData();
+  // See https://github.com/fengyuanchen/cropperjs#options
+  config = {
+    aspectRatio: 1,
+    initialAspectRatio : 1,
+    cropBoxResizable: false,
+    responsive: true,
+
+  };
+
+  constructor(private fb : FormBuilder, private httpClient : HttpClient) {
+  }
+
+  @ViewChild("fileInput", { static: true }) public fileInput: ElementRef;
+  @ViewChild("angularCropper", { static: true }) public angularCropper: CropperComponent;
+
+  onFileChange(event) {
+    let reader = new FileReader();
+    if(event.target.files && event.target.files.length > 0) {
+      let file = event.target.files[0];
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        this.form.get('avatar').setValue({
+          filename: file.name,
+          filetype: file.type,
+          value: reader.result.toString().split(",")[1]
+        })
+        
+        if (/^image\/\w+/.test(file.type)) {
+          var newUrl = URL.createObjectURL(file);
+          this.angularCropper.imageElement.src = newUrl;
+          this.angularCropper.cropper.destroy();
+        } else {
+          window.alert("Please choose an image file..");
+        }
+      };
+    }
+  }
+
+  onSubmit() {
+    this.angularCropper.cropper.getCroppedCanvas().toBlob((blob) => {
+
+      // Send the image as a binary to image service
+      // the image services only uses stream for receiving the file
+      // so this reduces the overhead of having to send it as a base-64string
+      // https://www.davidbcalhoun.com/2011/when-to-base64-encode-images-and-when-not-to/
+      this.loading = true;
+  
+      this.httpClient.post("http://localhost:5000/upload/d4620538-4364-44d2-8377-c9c84ee3df44", blob).toPromise()
+      .then((ok) => {
+          if(ok) {
+            window.alert(`Success, image id: ${ok["imageId"]}`);
+          }
+          this.loading = false;
+        })
+        .catch((err: HttpErrorResponse) => {
+          window.alert(`${err.message}`)
+          this.loading = false;
+      });
+    });
+  }
+
+  clearFile() {
+    this.form.get('avatar').setValue(null);
+    this.fileInput.nativeElement.value = '';
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CropitModule } from 'cropit'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
     declarations: [AppComponent],
     imports: [
+        FormsModule,
+        CropitModule,
         BrowserModule,
-        CropitModule
+        HttpClientModule,
+        ReactiveFormsModule,
     ],
     providers: [],
     bootstrap: [AppComponent]
